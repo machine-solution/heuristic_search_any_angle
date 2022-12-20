06a77da8b7699462ad7f0dd06dffce910e2deee6
Maria <radmarale@gmail.com>
2022-12-20 18:26:50 +0300

    utils updated

--- _2k_astar/utils.py
+++ _2k_astar/utils.py
@@ -92,7 +92,6 @@ def h_2k(i1, j1, i2, j2, k):
     return x * length(*l) + y * length(*r)
 
 
-# using for counting statistics and returning it as one variable
 class Stats:
     def __init__(self):
         self.difficulty = 0
@@ -101,6 +100,27 @@ class Stats:
         self.way_length = 0  # algorithm must set this value
         self.suboptimal = 0
         self.max_tree_size = 0  # algorithm must set this value
+
+    def read_from_string(self, data):
+        delimiter = ","
+        diff, expansions, runtime, way_length, suboptimal, tree_size = data.split(delimiter)
+        self.difficulty = int(diff)
+        self.expansions = int(expansions)
+        self.runtime = float(runtime)
+        self.way_length = float(way_length)
+        self.suboptimal = int(suboptimal)
+        self.max_tree_size = int(tree_size)
+
+    def __repr__(self):
+        delimiter = ","
+        return str(self.difficulty) + delimiter + str(self.expansions) + delimiter + str(
+            self.runtime) + delimiter + str(self.way_length) + \
+               delimiter + str(self.suboptimal) + delimiter + str(self.max_tree_size)
+
+    def header(self):
+        delimiter = ","
+        return "difficulty" + delimiter + "expansions" + delimiter + "runtime" + delimiter + "way_length" + \
+               delimiter + "suboptimal" + delimiter + "max_tree_size"
 
 
 class SearchTreePQS:  # SearchTree which uses PriorityQueue for OPEN and set for CLOSED
@@ -154,11 +174,11 @@ def make_path(goal):
     It also returns path's length.
     '''
 
-    length = goal.g
+    length_ = goal.g
     current = goal
     path = []
     while current.parent:
         path.append(current)
         current = current.parent
     path.append(current)
-    return path[::-1], length
+    return path[::-1], length_
